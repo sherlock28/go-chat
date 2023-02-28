b5e9640e59a9ce5d7b898163e70a44fd90af9449
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-02-28 09:09:41 -0300

    Added: 'CreateUserReq', 'CreateUserRes' structs and 'Service' interface

--- server/internal/user/user.go
+++ server/internal/user/user.go
@@ -9,6 +9,22 @@ type User struct {
 	Password string `json:"password" db:"password"`
 }
 
+type CreateUserReq struct {
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
+}
+
+type CreateUserRes struct {
+	ID       string `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+}
+
 type Repository interface {
 	CreateUser(ctx context.Context, user *User) (*User, error)
+}
+
+type Service interface {
+	CreateUser(ctx context.Context, req *CreateUserReq) (*CreateUserRes, error)
 }
